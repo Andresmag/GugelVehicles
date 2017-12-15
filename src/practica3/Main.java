6c05202c2d30113a83cfd1db3aa2a4048031c24b
Joselmo <joselmo@correo.ugr.es>
2017-12-15 12:11:27 +0100

    Commit de prueba de Jose

--- src/practica3/Main.java
+++ src/practica3/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // Comit de prueba de Diego
         System.out.println("Hello World!");
+        System.out.println("Hi!");// Commit de prueba de Jose
     }
 }
