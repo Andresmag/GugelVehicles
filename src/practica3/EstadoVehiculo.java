a8f6ea2c7572f9a9da10eaf98d7f5af969472d0f
Joselmo <joselmo@correo.ugr.es>
2017-12-22 12:03:44 +0100

    Añadida EstadoVehiculo

---
+++ src/practica3/EstadoVehiculo.java
@@ -0,0 +1,33 @@
+package practica3;
+
+import es.upv.dsic.gti_ia.core.AgentID;
+
+public class EstadoVehiculo {
+
+    public AgentID id;
+    public int coor_x,coor_y;
+    public boolean objetivoAlcanzado;
+    public int battery;
+    public TipoVehiculo tipoVehiculo;
+
+    public EstadoVehiculo(AgentID id) {
+        this.id = id;
+    }
+
+    public EstadoVehiculo(AgentID id, int battery, TipoVehiculo tipoVehiculo) {
+        this.id = id;
+        this.battery = battery;
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public EstadoVehiculo(AgentID id, int coor_x, int coor_y, boolean objetivoAlcanzado, int battery, TipoVehiculo tipoVehiculo) {
+        this.id = id;
+        this.coor_x = coor_x;
+        this.coor_y = coor_y;
+        this.objetivoAlcanzado = objetivoAlcanzado;
+        this.battery = battery;
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+
+}
---
+++ src/practica3/TipoVehiculo.java
@@ -0,0 +1,5 @@
+package practica3;
+
+public enum TipoVehiculo {
+    dron,coche,camion
+}
